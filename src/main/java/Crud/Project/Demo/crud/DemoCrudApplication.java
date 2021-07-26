74c57c80e6bb08bf92dff21721d6ba36b2699ff1
swati kulkarni <swatikulkarni89@gmail.com>
2021-07-26 17:48:55 +0530

    crud opration

---
+++ src/main/java/Crud/Project/Demo/crud/DemoCrudApplication.java
@@ -0,0 +1,15 @@
+package Crud.Project.Demo.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCrudApplication.class, args);
+		
+		System.out.println("hellow world");
+	}
+
+}
---
+++ src/test/java/Crud/Project/Demo/crud/DemoCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package Crud.Project.Demo.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
